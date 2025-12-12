4b4dc3dd8d65fa07d225b16faa84fb60cfcba298
germanClaudio <gmontalbetti@prodismo.com>
2025-12-12 15:53:12 -0300

    [UPDATE] Archivos generales_12-12-2025#1

--- js/config.generated.js
+++ js/config.generated.js
@@ -1,5 +1,5 @@
 // ARCHIVO GENERADO AUTOMÁTICAMENTE - NO EDITAR MANUALMENTE
-    // Generated: 2025-12-08T12:35:01.795Z
+    // Generated: 2025-12-12T16:55:27.084Z
     // Environment: development
     window.CONFIG = {
   "ACCESS_CODES": [
@@ -20,6 +20,6 @@
     "D9$sXw@eFv2#rTb!5NmKj"
   ],
   "ENV": "development",
-  "TIMESTAMP": "2025-12-08T12:35:01.794Z"
+  "TIMESTAMP": "2025-12-12T16:55:27.083Z"
 };
     
