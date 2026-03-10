0adf473a5a1e212a61be051ccbaaffbacd36c2e5
germanClaudio <gmontalbetti@prodismo.com>
2026-03-10 08:41:48 -0300

    [UPDATE] Sidebar Procedimientos_10-3-2026#1

--- js/config.generated.js
+++ js/config.generated.js
@@ -1,5 +1,5 @@
 // ARCHIVO GENERADO AUTOMÁTICAMENTE - NO EDITAR MANUALMENTE
-    // Generated: 2026-03-09T10:50:12.481Z
+    // Generated: 2026-03-10T10:46:36.407Z
     // Environment: development
     window.CONFIG = {
   "ACCESS_CODES": [
@@ -20,6 +20,6 @@
     "D9$sXw@eFv2#rTb!5NmKj"
   ],
   "ENV": "development",
-  "TIMESTAMP": "2026-03-09T10:50:12.480Z"
+  "TIMESTAMP": "2026-03-10T10:46:36.406Z"
 };
     
